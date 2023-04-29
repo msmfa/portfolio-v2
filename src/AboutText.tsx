4eab45d2d950df75abe6ba47ed1285e1cdfae983
michael-moore <michaelsydneymoore@hotmail.com>
2023-04-29 15:49:18 +0100

    chore: update copy

--- src/AboutText.tsx
+++ src/AboutText.tsx
@@ -1,31 +1,38 @@
-import React from "react";
-
 export default function AboutText() {
   return (
     <section>
       <p>
-        I'm Michael Sydney Moore. Software Engineer. Currently a lead frontend
-        developer at{" "}
+        I'm Michael. A Software Engineer from London.
+        <br />
+        Currently a lead frontend developer at{" "}
         <a href="https://www.browserlondon.com/" target="blank">
-          Browser London.
+          Browser.
         </a>
       </p>
       <p>
         If I'm not at work I'm probably working on{" "}
-        <a href="https://www.sober-app.co.uk/">Sober App.</a> A dating App for
-        sober people. A React Native app with a Mongo backend.
+        <a href="https://www.sober-app.co.uk/" target="blank">
+          Sober App.
+        </a>
+        <br />A dating App for sober people. (React Native + Express + Mongo)
       </p>
       <p>
         Want to follow my journey in launching Sober App? I publish a bi-monthly
-        newsletter about my success and failings.{" "}
-        <a href="https://faileveryday.substack.com/p/documenting-every-failure-in-my-business">
+        newsletter about my success and failings.
+        <br />
+        <a
+          href="https://faileveryday.substack.com/p/documenting-every-failure-in-my-business"
+          target="blank"
+        >
           Documenting every failure in my business in the first 14 days
         </a>
       </p>
       <p>
         In my spare time I like{" "}
-        <a href="https://www.instagram.com/michaelsydneymoore">painting</a> and
-        boxing (not at the same time).
+        <a href="https://www.instagram.com/michaelsydneymoore" target="blank">
+          painting
+        </a>{" "}
+        and boxing (not at the same time).
       </p>
     </section>
   );
