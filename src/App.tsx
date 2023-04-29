12a3a2e383bcf0d27c64248f7c1f1be0d72eaf16
michael-moore <michaelsydneymoore@hotmail.com>
2023-04-29 15:16:05 +0100

    feat: add about with links

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,31 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p>App</p>
+      <p>
+        I'm Michael Sydney Moore. Software Engineer. Currently a lead frontend
+        developer at{" "}
+        <a href="https://www.browserlondon.com/" target="blank">
+          Browser London.
+        </a>
+      </p>
+      <p>
+        If I'm not at work I'm probably working on{" "}
+        <a href="https://www.sober-app.co.uk/">Sober App.</a> A dating App for
+        sober people. A React Native app with a Mongo backend.
+      </p>
+
+      <p>
+        Want to follow my journey in launching Sober App? I publish a bi-monthly
+        newsletter about my success and failings, here's the first post{" "}
+        <a href="https://faileveryday.substack.com/p/documenting-every-failure-in-my-business">
+          Documenting every failure in my business in the first 14 days
+        </a>
+      </p>
+
+      <p>
+        In my spare time I like to play chess and box, not at the same time (yes
+        that is a thing)
+      </p>
     </>
   );
 }
