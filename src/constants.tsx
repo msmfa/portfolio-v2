95a83cf906d15d9242981358d9cd561bbf2e6cdd
Michael Sydney Moore <codemoore@outlook.com>
2023-07-31 15:57:27 +0100

    chore: update CV

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,6 +1,6 @@
 export default {
   github: "https://github.com/msmfa",
   email: "mailto:codemoore@outlook.com",
-  pdf: "/public/resume.pdf",
+  pdf: "/public/CV-2023.pdf",
   instagram: "https://www.instagram.com/michaelsydneymoore",
 };
